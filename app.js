922943c571bd032c270b7bc88fb56f323126127a
MJuneja29 <mjuneja.work@gmail.com>
2025-09-24 23:52:24 +0530

    finalsized JS file

---
+++ app.js
@@ -0,0 +1,93 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["yellow","red","purple","green"];
+
+let started = false;
+let level = 0;
+
+h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function() {
+    if(started == false) {
+        console.log("game is started!");
+        started = true;
+    }
+
+    levelUp();
+});
+
+function gameFlash(btn) {
+    setTimeout(function() {
+        btn.classList.add("flash")
+    },500);
+    setTimeout(function() {
+        btn.classList.remove("flash")
+    }, 850);
+} 
+
+function userFlash(btn) {
+    btn.classList.add("userflash");
+    setTimeout(function() {
+        btn.classList.remove("userflash")
+    }, 250);
+}
+
+
+function levelUp() {
+    userSeq = [];
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * 3);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`)
+    gameSeq.push(randColor);
+    console.log(gameSeq)
+    gameFlash(randBtn);
+}
+
+
+function checkAns(idx) {
+    // console.log(`Current level:, ${level}`)
+
+    if(userSeq[idx] === gameSeq[idx]) {
+        if(userSeq.length == gameSeq.length) {      // user has clicked the same values
+            setTimeout(levelUp(), 1000);
+        }
+        // console.log("same value")
+    } else {
+        h2.innerHTML = `Game over! Your score was <b>${level}</b><br>Press any key to start`
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function() {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150)
+        reset();
+    }
+}
+
+
+function btnPress() {
+    let btn = this;
+    userFlash(btn);
+    // console.log(this)
+
+    userColor = btn.id
+    // console.log(userColor)
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length - 1);       // checking the last index of userSeq
+}
+
+allBtns = document.querySelectorAll(".btn");
+
+for(btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
